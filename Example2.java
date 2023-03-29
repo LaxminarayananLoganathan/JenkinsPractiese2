b2633b49ae1134a077150ee83528f26eb2eb2ae1
LaxminarayananLoganathan <124677287+LaxminarayananLoganathan@users.noreply.github.com>
2023-03-29 09:21:54 +0530

    Update Example2.java

--- Example2.java
+++ Example2.java
@@ -7,5 +7,6 @@ System.out.println("I am Laxminarayanan");
 System.out.println("From Krishnagiri");
 System.out.println("Completed BE");
 System.out.println("In Coimbatore");
+  System.out.println("Now from Hosur");
 }
 }

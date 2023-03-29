9a8b15bf9978862b223e68bf8d6390c4fd5ed87a
LaxminarayananLoganathan <124677287+LaxminarayananLoganathan@users.noreply.github.com>
2023-03-29 09:16:32 +0530

    Uploaded

---
+++ Example2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Example2
+{
+public static void main(String[]args)
+{
+System.out.println("I am Laxminarayanan");
+System.out.println("From Krishnagiri");
+System.out.println("Completed BE");
+System.out.println("In Coimbatore");
+}
+}
